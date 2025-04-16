f1b49e4d6484d95bde4970d68943240b6ee60ece
filip-ivanovic-igt-com <filip.ivanovic2@igtplc.onmicrosoft.com>
2025-04-16 20:21:33 +0200

    building API

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,29 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
 const app = express();
 
-app.post("/", (req, res) => {
-  res.send("Server is ready!")
+app.post("/products", async (req, res) => {
+  const product = req.body; // user will send this data
+
+  if (!product.name || !product.price || !product.image) {
+    return res.status(400).json({ success: false, message: 'Please provide all required fields' });
+  }
+  const newProduct = new Product(product);
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, product: newProduct });
+  } catch (error) {
+    console.log(`Error in create product: ${error.message}`);
+    res.status(500).json({ success: false, message: `Server Error: ${error.message}` });
+  }
 })
 
-console.log(process.env.MONGO_URI)
+
 
 app.listen(5000, () => {
   connectDB()
