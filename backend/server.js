c630c68bb1aa9d8c670601b3676a1716cec650f5
filipivanovic <filip_ivanovic@hotmail.com>
2025-04-17 20:24:24 +0200

    finishing building API

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,13 @@ import productsRoute from "./routes/products.route.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); //allows us to accept JSON data in req.body (middleware)
 
 app.use("/api/products", productsRoute)
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB()
-  console.log('Express server listening on port 5000!...');
+  console.log(`Express server listening on port ${PORT}...`);
 });
