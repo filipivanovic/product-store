1d0c64b56d11b323991e69b285741ae2ac9dbd90
filipivanovic <filip_ivanovic@hotmail.com>
2025-04-18 19:13:53 +0200

    fixes for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,25 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from "./config/db.js";
 import productsRoute from "./routes/products.route.js";
+import * as path from "node:path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); //allows us to accept JSON data in req.body (middleware)
 
 app.use("/api/products", productsRoute)
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, ".frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, ".frontend/dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB()
