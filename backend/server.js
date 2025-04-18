c9ed3b09785ca7f0743c14bcf3ddd6ca1a0f7270
filipivanovic <filip_ivanovic@hotmail.com>
2025-04-18 19:27:13 +0200

    fix for server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from "./config/db.js";
 import productsRoute from "./routes/products.route.js";
-import * as path from "node:path";
+import path from "path";
 
 dotenv.config();
 
